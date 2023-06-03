2bb8d8b9edaa7cfcc53943ba656c8e5df9364abc
ahsan-rigu <114474110+ahsan-rigu@users.noreply.github.com>
2023-06-04 03:31:25 +0530

    fix: clear filters when browse all is clicked

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -3,8 +3,13 @@ import HeroSlider from "./components/HeroSlider";
 import HomeGrids from "./components/HomeGrids";
 
 const Home = () => {
+  const { dispatchFilterInput } = useContext(SessionContext);
+
   useEffect(() => {
     window.scrollTo(0, 0);
+    dispatchFilterInput({
+      action: "CLEAR_ALL_FILTERS",
+    });
   }, []);
   return (
     <div>
